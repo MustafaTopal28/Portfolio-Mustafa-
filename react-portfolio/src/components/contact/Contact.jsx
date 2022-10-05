45dc3e7b1c018b9a02bcd38e3437ea0db19b70a3
Mustafa Topal <mustafatopal@MacBook-Pro-de-Mustafa.local>
2022-10-05 03:04:53 +0200

    Finishing the last components such as the portfolio section, testimonials, contact and the footer. And change the color when clicking on the navbar.

--- react-portfolio/src/components/contact/Contact.jsx
+++ react-portfolio/src/components/contact/Contact.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_f8pn8rp', 'template_0io5ups', form.current, '5C5ilSTXMDgAkiOOi')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset()
+  };
+
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className="contact__option-icon"/>
+            <h4>Email</h4>
+            <h5>mustafatopalpro@gmail.com</h5>
+            <a href="mailto:mustafatopalpro@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine className="contact__option-icon"/>
+            <h4>Messenger</h4>
+            <h5>Mustafa Twopal</h5>
+            <a href="https://m.me/profile.php?id=100006410898543" target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className="contact__option-icon"/>
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a href="https://wa.me/+33768740697" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'> Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
